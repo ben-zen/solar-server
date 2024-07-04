996fa6f07d138eff0ab41127d0d6c98344937649
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2024-07-04 15:37:45 -0700

    First steps to a logger utility.

---
+++ logger/src/main.cc
@@ -0,0 +1,41 @@
+#include <chrono>
+#include <format>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sys/sysinfo.h>
+
+
+std::map<std::string, std::string> generate_report()
+{
+    std::map<std::string, std::string> report{};
+    struct sysinfo s_info{};
+
+    int err = sysinfo(&s_info);
+    if (err != 0)
+    {
+        std::cerr << "Error reading sysinfo: " << err << std::endl;
+    }
+    else
+    {
+        std::chrono::seconds uptime{s_info.uptime};
+        report.emplace("uptime", std::format("{:%T}", uptime));
+    }
+
+
+    report.emplace("temp", "65");
+    report.emplace("voltage", "12.7");
+    report.emplace("charging", "false");
+    
+    return report;
+}
+
+int main(int argc, char **argv)
+{
+    auto report = generate_report();
+
+    for (auto &&row : report)
+    {
+        std::cout << row.first << ": " << row.second << std::endl;
+    }
+}
