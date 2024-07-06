6c8ba55079d85193a40b47c62fdc9d07ad5dc028
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2024-07-06 01:42:40 -0700

    Added JSON handling and started building the structures I need.

--- logger/README.md
+++ logger/README.md
@@ -4,3 +4,8 @@
 
 Ensure you've got `libcurl` and `libcurl-devel` packages installed (for Fedora, etc.)--on other platforms, whatever the headers package is will be the correct choice.
 
+## Other works
+
+### nlohmann's json.hpp
+
+I'm using [nlohmann's json library](https://github.com/nlohmann/json) to handle json parsing.
--- logger/src/weather.cc
+++ logger/src/weather.cc
@@ -1,10 +1,16 @@
+#include <algorithm>
+#include <exception>
+#include <format>
 #include <iostream>
 #include <string>
 #include <sstream>
 
 #include <curl/curl.h>
 
+#include "include_ext/json.hpp"
 #include "weather.hh"
+
+using json = nlohmann::json;
 
 std::string get_observation_url()
 {
@@ -55,6 +61,38 @@ size_t gather_response(void *buffer,
     return nmemb;
 }
 
+enum class overall_condition {
+    clear,
+    partly_cloudy,
+    cloudy,
+    misting,
+    raining,
+    snowing
+};
+
+overall_condition condition_from_string(const std::string &str) {
+    static std::map<std::string, overall_condition> relation {
+        {"Sunny", overall_condition::clear},
+        {"Clear", overall_condition::clear},
+    };
+
+    if (!relation.contains(str))
+    {
+        throw new std::domain_error {std::format("incorrect condition supplied: {}", str)};
+    }
+
+    return relation[str];
+}
+struct daytime_forecast {
+    std::string timeframe;
+    overall_condition condition;
+    float temperature;
+
+    std::string formatted() {
+        return std::format("{}, {}, {}", timeframe, (int)condition, temperature);
+    }
+};
+
 std::string get_weather_data()
 {
     CURL *curl = curl_easy_init();
@@ -63,7 +101,7 @@ std::string get_weather_data()
         std::cerr << "Error setting up curl." << std::endl;
         return "";
     }
-    std::string forecast{};
+    std::string forecast_str{};
     CURLcode res;
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "solar-website vNone, https://ben.zen.sdf.org");
     curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "application/geo+json");
@@ -72,7 +110,7 @@ std::string get_weather_data()
                      gather_response);
     curl_easy_setopt(curl,
                      CURLOPT_WRITEDATA,
-                     &forecast);
+                     &forecast_str);
     auto forecast_url = get_forecast_url();
     curl_easy_setopt(curl, CURLOPT_URL, forecast_url.c_str());
     std::cout << "Set URL to: " << forecast_url << std::endl;
@@ -83,12 +121,42 @@ std::string get_weather_data()
         return "";
     }
 
-    std::cout << "Got full weather api response: " << std::endl
-              << forecast << std::endl;
-
     // We got JSON. Now let's extract something from it.
+    json forecast_data = json::parse(forecast_str);
+    auto forecast = forecast_data["properties"]["periods"];
 
-    
+    auto forecast_iter = forecast.cbegin();
+    // where entries have "isDaytime": true, take three.
+    std::vector<daytime_forecast> daytime_forecasts{};
+    auto is_daytime = [](json j) {
+        std::cout << "is_daytime(): " << j.contains("isDaytime") << " , " << j["isDaytime"] << std::endl;
+        return j.contains("isDaytime") && j["isDaytime"].template get<bool>();
+    };
+
+    do {
+        auto forecast_local = std::find_if(forecast_iter, forecast.cend(), is_daytime);
+
+        if (forecast_local == forecast.cend())
+        {
+            std::cerr << "ran out of daytimes." << std::endl;
+            break;
+        }
+
+        std::cout << "forecast found: " << *forecast_iter << std::endl;
+
+        daytime_forecasts.emplace_back(forecast_local->at("startTime").template get<std::string>(),
+                                       condition_from_string(
+                                            forecast_local->at("shortForecast").template get<std::string>()),
+                                       forecast_local->at("temperature").template get<float>());
+
+        // skip past the current 
+        forecast_iter = ++forecast_local;
+    } while (daytime_forecasts.size() < 3 && forecast_iter != forecast.cend());
+
+    for (auto &&f : daytime_forecasts)
+    {
+        std::cout << std::format("{}, {}, {}", (int)f.condition, f.temperature, f.timeframe) << "," << std::endl;
+    }
 
     curl_easy_cleanup(curl);
     return "69";
