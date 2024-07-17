17fe81792201d1320ca3987f6b01d5ed96585cef
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2024-07-17 00:16:28 -0700

    Guestbook: started using the arg parser.

--- guestbook/src/main.cc
+++ guestbook/src/main.cc
@@ -7,7 +7,10 @@
 #include <ranges>
 #include <string>
 
+#include <unistd.h>
+
 #include <json.hpp>
+#include <argparse.hpp>
 
 using json = nlohmann::json;
 
@@ -33,29 +36,56 @@ int format_entry(const std::string &author,
 
 int main(int argc, char **argv) {
 
+    auto &err = std::cout;
+    std::string author{};
+    std::string location{};
+    std::string message{};
     // auto content_length = getenv("CONTENT_LENGTH");
     // if (content_length == nullptr) {
     //     // Not running as a CGI script.
-    //     std::cerr << "Not running as CGI. Exit for now." << std::endl;
+    //     err << "Not running as CGI. Exit for now." << std::endl;
     //     return 1;
     // }
 
-    // std::cerr << "Running as CGI. Continue." << std::endl;
+    // err << "Running as CGI. Continue." << std::endl;
 
-    std::cerr << std::format("Received {} arguments: ", argc);
-    for (int i = 0; i < argc; i++) {
-        std::cerr << argv[i] << ", ";
+    argparse::ArgumentParser arg_parser{"guestbook", "0.1.0"};
+    arg_parser.add_argument("--author").help("The author of the post");
+    arg_parser.add_argument<std::string>("--location").help("The author's affiliation or original location");
+    arg_parser.add_argument<std::string>("--message").help("The actual message for the guestbook.");
+    arg_parser.add_argument<std::string>("-o", "--output").help("The file to write the guestbook entry to.");
+
+    try {
+        arg_parser.parse_args(argc, argv);
+    } catch (const std::exception &e) {
+        std::cerr << "Exception parsing arguments: " << e.what() << std::endl;
+        std::cerr << arg_parser << std::endl;
+        std::exit(1);
     }
-    std::cerr << std::endl;
 
-    if (argc < 4) {
-        std::cerr << "Needs three arguments (author, location, message) to build a guestbook entry." << std::endl;
-        return 1;
+
+
+    err << "Environment variables:" << std::endl;
+    for(char **env_var = environ; *env_var != nullptr; env_var++)
+    {
+        auto env_value = std::getenv(*env_var);
+        err << '\t' << std::string{*env_var} << ": "
+                  << ((env_value != nullptr) ? env_value : "NULL") << std::endl;
+    }
+    err << std::endl;
+
+    try {
+        author = arg_parser.get<std::string>("--author");
+        location = arg_parser.get<std::string>("--location");
+        message = arg_parser.get<std::string>("--message");
+    } catch (const std::exception &e) {
+        std::cerr << "Exception reading variables: " << e.what() << std::endl;
+        std::exit(2);
     }
 
-    format_entry(argv[1], argv[2], argv[3], std::cout);
+    format_entry(author, location, message, std::cout);
 
-    std::cerr << "If the message is incomplete, try wrapping it in quotes!" << std::endl;
+    err << "If the message is incomplete, try wrapping it in quotes!" << std::endl;
 
-    return 0;
+    std::exit(0);
 }
